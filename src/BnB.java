c747ada6bab9ef8ba2376e5f0e30d06933186375
Yunwei Qiang <yunweiqiang@lawn-128-61-76-76.lawn.gatech.edu>
2017-11-11 15:28:13 -0500

    back tracking BnB

--- src/BnB.java
+++ src/BnB.java
@@ -26,7 +26,7 @@ class BnB{
 		return true;
 	}
 
-	public int DFS(){
+	public int DFS(int index){
 		if(checkTime){
 			return 0;
 		}
@@ -36,41 +36,59 @@ class BnB{
 			return 0;
 		}
 
-		int index;
-		for(int i=0; i<G.vertexCovered.length; i++){//choose a vertex not pruned or selected
+		// int index;
+		for(int i=index+1; i<G.vertexCovered.length; i++){//choose a vertex not pruned or selected
 			if(!G.vertexCovered[i]){
 				index = i;
-				break;
+				//make copy before we mess around
+				// boolean[] copyVertexCovered = Arrays.copyOf(vertexCovered, vertexCovered.length);
+				// boolean[] copyEdgeCovered = Arrays.copyOf(edgeCovered, edgeCovered.length);
+
+				//make a stack to store changed vertices and edges
+				Stack<Integer> vertexStack = new Stack<Integer>();
+				Stack<Integer> edgeStack = new Stack<Integer>();
+
+				Vertex currentChoice = G.getVertex(index);//the vertex we choose to add in solution
+				currentSolution.add(currentChoice);
+				G.vertexCovered[index] = true;//first change
+				vertexStack.push(index);
+
+				int[] keys = currentChoice.getEdgeKeys();//get all edges' keys linked to this vertex 
+				for(int key:keys){
+					Edge temp = edgeMap.get(key);//find correspond edge in HashMap by key
+					if(!edgeCovered[temp.id]){//change to key later
+						edgeCovered[temp.id] = true;//mark this edge covered based on id
+						edgeStack.push(temp.id);
+					}
+					//prune vertices
+					Vertex another = temp.getAnother();//get adjacent vertex of this edge "temp"
+					if(!vertexCovered[another.id]){
+						int[] keysAnother = another.getEdgeKeys();//get all the edges' keys of vertex "another"
+						boolean tempBoolean = true;
+						for(int keyAnother:keysAnother){
+							Edge tempAnother = edgeMap.get(keyAnother);//find correspond edge in HashMap by keyAnother
+							tempBoolean = tempBoolean & edgeCovered[tempAnother.id];
+							// if(!edgeCovered[tempAnother.id]) break;//if any edge has not been covered, break
+						}
+						vertexCovered[another.id] = tempBoolean;
+						if(tempBoolean) vertexStack.push(another.id);
+					}
+				}
+
+				DFS(index);
+				
+				//restore what we messed
+				// vertexCovered = copyVertexCovered;
+				// edgeCovered = copyEdgeCovered;
+				while(!vertexStack.empty()){
+					vertexCovered[vertexStack.pop()] = false;
+				}
+				while(!edgeStack.empty()){
+					edgeCovered[edgeStack.pop()] = false;//change to key later
+				}
 			}
 		}
-
-		//make copy before we mess around
-		boolean[] copyVertexCovered = Arrays.copyOf(vertexCovered, vertexCovered.length);
-		boolean[] copyEdgeCovered = Arrays.copyOf(edgeCovered, edgeCovered.length);
-
-		Vertex currentChoice = G.getVertex(index);//the vertex we choose to add in solution
-		currentSolution.add(currentChoice);
-		int[] keys = currentChoice.getEdgeKeys();//get all edges' keys linked to this vertex 
-		for(int key:keys){
-			Edge temp = edgeMap.get(key);//find correspond edge in HashMap by key
-			edgeCovered[temp.id] = true;//mark this edge covered based on id
-			//prune vertices
-			Vertex another = temp.getAnother();//get adjacent vertex of this edge "temp"
-			int[] keysAnother = another.getEdgeKeys();//get all the edges' keys of vertex "another"
-			boolean tempBoolean = true;
-			for(int keyAnother:keysAnother){
-				Edge tempAnother = edgeMap.get(keyAnother);//find correspond edge in HashMap by keyAnother
-				tempBoolean = tempBoolean && edgeCovered[tempAnother.id];
-				// if(!edgeCovered[tempAnother.id]) break;//if any edge has not been covered, break
-			}
-			vertexCovered[another.id] = tempBoolean;
-		}
-
-		DFS();
-		
-		//restore what we messed
-		vertexCovered = copyVertexCovered;
-		edgeCovered = copyEdgeCovered;
+		return 1;
 
 	}
 
