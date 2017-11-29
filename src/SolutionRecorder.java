66e3cc8c5bcd9f786669eca016b18624ef45838f
saka <asclepius1993@gmail.com>
2017-11-29 10:31:07 -0500

    add Recorder class to do output

---
+++ src/SolutionRecorder.java
@@ -0,0 +1,47 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+	
+public class SolutionRecorder {
+	private static final SolutionRecorder sr = new SolutionRecorder();
+    private  String currDir = System.getProperty("user.dir");
+    private  String hcOutFilePath;
+    private  String hcTraceFilePath;
+    private  PrintWriter hcOut;
+    private  PrintWriter hcTrace;
+	
+	public void configure(String filePath,String alg,String randomSeed,String cutoffTime) throws IOException {
+		String[] elements = filePath.split("\\.|/");
+		this.hcOutFilePath = this.currDir + "/output/" + elements[elements.length-2] + "_"+alg+"_"+cutoffTime+"_"+
+				randomSeed+".sol";
+		this.hcTraceFilePath = this.currDir + "/output/" + elements[elements.length-2] + "_"+alg+"_"+cutoffTime+"_"+
+				randomSeed+".trace";
+		this.hcOut = new PrintWriter(this.hcOutFilePath);
+        this.hcTrace = new PrintWriter(this.hcTraceFilePath);
+	}
+    
+	public void printSolution(List<Vertex> optimalSolution) {
+		hcOut.printf("%d%n", optimalSolution.size()); //write .sol file
+        for(int i =0; i < optimalSolution.size(); i++){
+            hcOut.printf("%s",optimalSolution.get(i).getId());
+            if(i != optimalSolution.size()-1){
+                hcOut.printf(",");
+            }
+        }
+        hcOut.close();
+        hcTrace.close();
+	}
+	
+	public void printTrace(String format,Object... args) {
+		hcTrace.printf(format,args);
+	}
+	
+	
+	public static SolutionRecorder getInstance() {
+		return sr;
+	}
+	
+	private SolutionRecorder() {}
+
+}
